9a23096e96bf6f79a17884e9c769f008693c5509
Itai Ktalav <itaiktalav@outlook.com>
2025-05-14 20:47:22 +1000

    Button now works

--- src/components/NusachPage.vue
+++ src/components/NusachPage.vue
@@ -1,18 +1,18 @@
 <script lang="ts" setup>
-  import NusachHebrew from './NusachHebrew.vue';
-  import NusachEnglishTransliteration from './NusachEnglishTransliteration.vue';
+    import NusachHebrew from './NusachHebrew.vue';
+    import NusachEnglishTransliteration from './NusachEnglishTransliteration.vue';
+    import { ref } from 'vue';
+    let showLeft = ref(true);
 </script>
 <template>
-  <h1>Self Pidyon - Nusach</h1>
-  <div class="columns">
-    <div class="column"><NusachEnglishTransliteration /></div>
-    <div class="column"><NusachHebrew /></div>
-  </div>
-
-  <h2>What is a "Self Pidyon"?</h2>
-  <p>Redemption of the firstborn is an important mitzvah from from the Torah which calls on a father to redeem a
-    firstborn son by paying 5 silver shekels to a Kohen.</p>
-  <p>Sometimes however, due to a number of circumstances this doesn't happen, and when the firstborn turns 13, the
-    obligation then falls upon himself to Redeem himself. IE. A "Self Pidyon"</p>
-  <p>This site provides information on how to conduct a Self Pidyon </p>
+    <h1>Self Pidyon - Nusach</h1>
+    <div class="columns">
+        <div v-if="showLeft" class="column">
+            <NusachEnglishTransliteration />
+        </div>
+        <div class="column">
+            <button v-on:click="showLeft = !showLeft" type="button">TOGGLE</button>
+            <NusachHebrew />
+        </div>
+    </div>
 </template>
