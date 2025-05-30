218ace15a905553c6076c43d694c32bfaf4b7eeb
Itai Ktalav <itaiktalav@outlook.com>
2025-05-30 18:31:18 +0300

    Added colours for light theme

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+    import { useDark } from "@vueuse/core";
+    const isDark = useDark();
+</script>
+
 <template>
-  <RouterView />
+    <div id="navTabs">
+        <button type="button" onclick="location.href='/'">Nusach</button>
+        <button type="button" onclick="location.href='/info'">Information</button>
+        <button type="button" onclick="location.href='/source'">Source</button>
+    </div>
+    <RouterView />
 </template>
