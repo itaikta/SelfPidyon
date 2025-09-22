973ed94d4d44d5da6c668adef87c4147acdb8d94
Itai Ktalav <itaiktalav@outlook.com>
2025-09-22 17:56:57 +0300

    Moar QoL

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
     import { usePreferredColorScheme, useToggle } from '@vueuse/core';
     import Breadcrumbs from './components/Breadcrumbs.vue';
-    const themeColour = usePreferredColorScheme();
-    const toggleThemeColour = useToggle(themeColour);
+    //const themeColour = usePreferredColorScheme();
+    //const toggleThemeColour = useToggle(themeColour);
 </script>
 
 <template>
     <div id="navTabs">
-        <button type="button"><RouterLink to="/">Nusach</RouterLink></button>
-        <button type="button"><RouterLink to="/info">Information</RouterLink></button>
-        <button type="button"><RouterLink to="/source">Source</RouterLink></button>
+        <RouterLink to="/"><button type="button">Nusach</button></RouterLink>
+        <RouterLink to="/info"><button type="button">Information</button></RouterLink>
+        <RouterLink to="/source"><button type="button">Source</button></RouterLink>
+        <!--<button type="button" @click="toggleThemeColour()">Toggle Theme</button>
+        <button type="button">{{ themeColour }}</button>-->
     </div>
     <Breadcrumbs />
     <RouterView />
--- src/components/NusachEnglishTranslation.vue
+++ src/components/NusachEnglishTranslation.vue
@@ -1,8 +1,9 @@
 <template>
     <p class="nusach comment">The Firstborn declares:</p>
-    <p class="nusach">I am the firstborn; the first fruit of the womb, and HaShem has decreed that the firstborn shall be redeemed (if the father passed away: 
-        and to my sorrow, my father passed before I was redeemed), and I was left with the obligation to redeem myself, as it is written: You shall redeem
-        your firstborn, vekarinan tipada, which is to say that I should redeem myself. An here I am! Set and ready to do as such!</p>
+    <p class="nusach">I am the firstborn; the first fruit of the womb, and HaShem has decreed that the firstborn shall
+        be redeemed (<i>if the father passed away:</i> and to my sorrow, my father passed before I was redeemed), and I
+        was left with the obligation to redeem myself, as it is written: You shall redeem your firstborn, and we read:
+        ‘You shall be redeemed’, which is to say that I should redeem myself. And here I am! Set and ready to do as such!</p>
     <br />
 
     <p class="nusach comment">The Kohen asks:</p>
@@ -14,7 +15,8 @@
     <br />
 
     <p class="nusach comment">The first born makes these two blessings:</p>
-    <p class="nusach">baruch ata adonai eloheinu melech ha'olam asher kidshanu bemitzotav vetzivanu al pidion habchor</p>
+    <p class="nusach">baruch ata adonai eloheinu melech ha'olam asher kidshanu bemitzotav vetzivanu al pidion habchor
+    </p>
     <p class="nusach">baruch ata adonai eloheinu melech ha'olam shehechayanu vekiamanu vehigi'anu lazman hazeh</p>
     <br />
 
