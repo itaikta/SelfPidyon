32a294bec6bd859b59a11c225c0e325f7e98a36c
Itai Ktalav <itai@ktalav.co>
2026-04-03 01:50:39 +0300

    Can now change the theme on the nusach page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-    //import { usePreferredColorScheme, useToggle } from '@vueuse/core';
     import Breadcrumbs from './components/Breadcrumbs.vue';
-    //const themeColour = usePreferredColorScheme();
-    //const toggleThemeColour = useToggle(themeColour);
 </script>
 
 <template>
@@ -10,8 +7,6 @@
         <RouterLink to="/"><button type="button">Nusach</button></RouterLink>
         <RouterLink to="/info"><button type="button">Information</button></RouterLink>
         <RouterLink to="/source"><button type="button">Source</button></RouterLink>
-        <!--<button type="button" @click="toggleThemeColour()">Toggle Theme</button>
-        <button type="button">{{ themeColour }}</button>-->
     </div>
     <Breadcrumbs />
     <RouterView />
--- src/components/NusachPage.vue
+++ src/components/NusachPage.vue
@@ -3,24 +3,44 @@
     import NusachEnglishTransliteration from './NusachEnglishTransliteration.vue';
     import NusachEnglishTranslation from './NusachEnglishTranslation.vue';
     import { ref } from 'vue';
+    import { usePreferredDark } from '@vueuse/core';
+    
     let showLeft = ref(true);
     let transliteration = ref(true);
+    let preferDark = ref(usePreferredDark().value);
+    let preferHighContrast = ref(true);
+
+    function getColumnsId(preferDark: boolean, preferHighContrast: boolean) {
+        if (preferDark) {
+            if (preferHighContrast) return 'columnsDarkHigh';
+            else return 'columnsDarkLow';
+        }
+        else {
+            if (preferHighContrast) return 'columnsLightHigh';
+            else return 'columnsLightLow';
+        }
+    }
 </script>
 <template>
     <h1>Self Pidyon - Nusach</h1>
     <button v-on:click="showLeft = !showLeft" type="button" style="margin-right: 5px;"><span v-if="showLeft">Hide</span><span v-if="!showLeft">Show</span> Left Column</button>
     <button v-if="showLeft" v-on:click="transliteration = !transliteration" type="button">Show <span v-if="transliteration">Translation</span><span v-if="!transliteration">Transliteration</span></button>
     <div id="backgroundSelector">
-        <button style="padding-right: 3px; border-radius: 10px 0 0 10px;">AAA</button>
-        <button style="padding-left: 5px; padding-right: 5px; border-radius: 0;">BBB</button>
-        <button style="padding-left: 3px; border-radius: 0 10px 10px 0;">CCC</button>
+        <button v-on:click="preferDark = !preferDark" style="padding-right: 4px; border-radius: 10px 0 0 10px;">
+            <span v-if="!preferDark">Theme: Light ☀️</span>
+            <span v-if="preferDark">Theme: Dark 🌙</span>
+        </button>
+        <button v-on:click="preferHighContrast = !preferHighContrast" style="padding-left: 4px; border-radius: 0 10px 10px 0;">
+            <span v-if="preferHighContrast">Contrast: High ⬆️</span>
+            <span v-if="!preferHighContrast">Contrast: Low ⬇️</span>
+        </button>
     </div>
-    <div class="columns">
-        <div v-if="showLeft" class="column">
+    <div :id="getColumnsId(preferDark, preferHighContrast)">
+        <div v-if="showLeft" class="column" style="padding-left: 15px;">
             <NusachEnglishTransliteration v-if="transliteration" />
             <NusachEnglishTranslation v-if="!transliteration" />
         </div>
-        <div class="column">
+        <div class="column" style="padding-right: 15px;">
             <NusachHebrew />
         </div>
     </div>
