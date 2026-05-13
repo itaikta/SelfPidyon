006f07aa4c55a22afa352a8b9f464c36eecb8148
Itai Ktalav <itai@ktalav.co>
2026-05-13 15:52:24 +0300

    Update info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,9 +2,31 @@
     <h1>Information and Questions</h1>
     <h2>What is a "Self Pidyon"?</h2>
     <p>Redemption of the firstborn is an important mitzvah from from the Torah which calls on a father to redeem a
-        firstborn son by paying 5 silver shekels to a Kohen.</p>
-    <p>Sometimes however, due to a number of circumstances this doesn't happen, and when the firstborn turns 13, the
-        obligation then falls upon himself to Redeem himself. IE. A "Self Pidyon"</p>
+        firstborn son by paying 5 silver sla'im (shekels) to a Kohen.</p>
+    <p>Sometimes however, due to a number of circumstances this doesn't happen, and when the firstborn reaches bar
+        mitzvah at 13, the obligation then falls upon himself to redeem himself. IE. A "Self Pidyon"</p>
+    <p>For general information on pidyon haben, I recommend these articles from <a
+            href="https://aish.com/redeeming-first-born/" target="_blank">Aish</a> and <a
+            href="https://www.star-k.org/articles/kashrus-kurrents/15409/the-mitzvah-of-pidyon-haben/"
+            target="_blank">Star K</a></p>
+    <p>The full <i>standard</i> ritual can be found in any siddur, usually somewhere after birkat hamazon (grace after
+        meals), and also in this helpful article from <a
+            href="https://www.chabad.org/library/article_cdo/aid/928176/jewish/The-Complete-Pidyon-Haben-Ceremony.htm"
+            target="_blank">Chabad</a>
+    </p>
+
+    <h2>What's the difference between a standard Pidyon HaBen and a "Self Pidyon"?</h2>
+    <p>The main difference is to who has the obligation, and needs to perform the ceremony. When a firstborn son is born
+        to parents who aren't kohanim or Levi'im, the obligation is on the father to perform the ceremony on the 30th
+        day after the son is born or anytime after if that deadline is missed for any reason.</p>
+    <p>However, if the firstborn reaches bar mitzvah at 13 and the father did not perform the ceremony, the obligation
+        falls upon the individual to redeem himself, resulting in a "Self Pidyon". In this case, the ceremony is a bit
+        different and won't be found in a siddur or in the Chabad article I linked above. That is what this site is for.
+        The main page of this side shows the the part of the ceremony which is different when the firstborn is redeeming
+        himself.</p>
     
-    <p style="margin-top:20px">This site provides information on how to conduct a Self Pidyon. For the complete ceremony text, see our <RouterLink to="/">Nusach page</RouterLink>, and for the halachic source, visit our <RouterLink to="/source">Source page</RouterLink>.</p>
+    <hr style="margin-top: 25px;" />
+    <p>This site provides information on how to conduct a Self Pidyon. For the complete ceremony
+        text, see our <RouterLink to="/">Nusach page</RouterLink>, and for the halachic source, visit our <RouterLink
+            to="/source">Source page</RouterLink>.</p>
 </template>
